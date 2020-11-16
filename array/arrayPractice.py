9e21a94983f809bcad5ebbd121cde533ab7a75e3
Robert Benavidez <murabitbenavidez@gmail.com>
2020-11-16 13:45:07 -0500

    big O: Traverse an array

---
+++ array/arrayPractice.py
@@ -0,0 +1,24 @@
+from array import *
+
+# 1. Create an array and traverse it.
+
+my_array = array('i', [1,2,3,4,5])
+
+# O(n)
+for i in my_array:
+    print(i)
+# 2. Access individual elements through indecies.
+# 3. Append any value to the array using append().
+# 4. Insert value in an array using insert() method.
+# 5. Extend python array using extend() method.
+# 6. Add items from list into array using fromlist() method.
+# 7. Remove any array element using remove() method.
+# 8. Remove last array element using pop() method.
+# 9. Fetch any element through its index using index() method.
+# 10. Reverse a python array using reverse() method. 
+# 11. Get array buffer information through buffer_info() method.
+# 12. Check for number of occurrences of an element using count() method.
+# 13. Convert array to string using tostring() method.
+# 14. Convert array to a python list with same elements using tolist() method.
+# 15. Append a string to char array using fromstirng() method.
+# 16. Slice elements from an array.
