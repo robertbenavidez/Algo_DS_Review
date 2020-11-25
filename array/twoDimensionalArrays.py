9b6e8be0ffb1729492a797ea10e9ad5b54091a28
Robert Benavidez <murabitbenavidez@gmail.com>
2020-11-24 23:08:39 -0500

    searching array function

--- array/twoDimensionalArrays.py
+++ array/twoDimensionalArrays.py
@@ -27,9 +27,22 @@ print(twoDArray)
 
 # accessElements(twoDArray, 2, 3)
 # Traversing a Two dimensional array Time O(n^2) Space O(1)
-def traverseTDArray(array):
-    for i in range(len(array)):         # O(mn)
-        for j in range(len(array[0])):  # O(n)
-            print(array[i][j])          # O(1)
+# def traverseTDArray(array):
+#     for i in range(len(array)):         # O(mn)
+#         for j in range(len(array[0])):  # O(n)
+#             print(array[i][j])          # O(1)
 
-traverseTDArray(twoDArray)
+# traverseTDArray(twoDArray)
+
+# searching fro an element in a two dimensional array time Time O(n^2) Space O(1)
+def searchTDArray(array, value):
+    for i in range(len(array)):                                         # O(mn )
+        print('i', i)
+        for j in range(len(array[0])):                                  # O(n)
+            print('i', i, 'j', j)
+            if array[i][j] == value:                                    # O(1)
+                return 'The value is at index '+str(i)+" "+str(j)
+    return 'this element is not found'
+
+print(searchTDArray(twoDArray, 15))
+
