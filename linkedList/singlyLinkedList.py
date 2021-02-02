88c25e1624992ad06f36bbf564475c765f089f52
Robert Benavidez <murabitbenavidez@gmail.com>
2021-02-02 13:00:35 -0500

    print SLL method

--- linkedList/singlyLinkedList.py
+++ linkedList/singlyLinkedList.py
@@ -9,29 +9,30 @@ class SLinkedList:
         self.head = None
         self.tail = None
     def __iter__(self):
+        node = self.head
         while node:
             yield node
             node = node.next
     # Insert into Linked List
-    def insertSLL(self, value, location):
-        newNode = Node(value)
-        # if no node exists
-        if self.head is None:
-            self.head = newNode
-            self.tail = newNode
-        # if a node exists
-        else:
-            # inserting to the front of the linked list
-            if location == 0:
-                newNode.next = self.head
-                self.head = newNode
-            elif location == 1:
-                newNode.next = Node
-                self.head =
+    # def insertSLL(self, value, location):
+    #     newNode = Node(value)
+    #     # if no node exists
+    #     if self.head is None:
+    #         self.head = newNode
+    #         self.tail = newNode
+    #     # if a node exists
+    #     else:
+    #         # inserting to the front of the linked list
+    #         if location == 0:
+    #             newNode.next = self.head
+    #             self.head = newNode
+    #         elif location == 1:
+    #             newNode.next = Node
+    #             self.head =
             
 
 
-singlyLinkedList = singlyLinkedList()
+singlyLinkedList = SLinkedList()
 print([node.value for node in singlyLinkedList])
 node1 = Node(1)
 node2 = Node(2)
--- recursion/sumofDigits.py
+++ recursion/sumofDigits.py
@@ -4,10 +4,9 @@
 
 def sumofDigits(n):
     assert n >= 0 and int(n) == n, 'The number must be a positive integer'
-    assert n >= 9, 'The number must be 10 or more'
     if n == 0:  # base case
         return 0
     else:
         return int(n % 10) + sumofDigits(int(n / 10)) # recursive case
 
-print(sumofDigits(9))
+print(sumofDigits(25))
