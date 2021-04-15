cd72a1e8617046fae24cd37e56c95aae20acc931
Robert Benavidez <murabitbenavidez@gmail.com>
2021-04-15 15:12:33 -0400

    SLL

--- linkedList/singlyLinkedList.py
+++ linkedList/singlyLinkedList.py
@@ -1,3 +1,6 @@
+# Time: O(n)
+# Space O(1)
+
 class Node:
     def __init__(self, value=None):
         self.value = value
@@ -14,29 +17,36 @@ class SLinkedList:
             yield node
             node = node.next
     # Insert into Linked List
-    # def insertSLL(self, value, location):
-    #     newNode = Node(value)
-    #     # if no node exists
-    #     if self.head is None:
-    #         self.head = newNode
-    #         self.tail = newNode
-    #     # if a node exists
-    #     else:
-    #         # inserting to the front of the linked list
-    #         if location == 0:
-    #             newNode.next = self.head
-    #             self.head = newNode
-    #         elif location == 1:
-    #             newNode.next = Node
-    #             self.head =
+    def insertSLL(self, value, location):
+        newNode = Node(value)
+        # if no node exists
+        if self.head is None:  # O(1)
+            self.head = newNode
+            self.tail = newNode
+        # if a node exists
+        else:
+            # inserting to the front of the linked list
+            if location == 0:  # O(1)
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:  # O(1)
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0 
+                while index < location - 1:   # O(n)
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
             
 
 
 singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
 print([node.value for node in singlyLinkedList])
-node1 = Node(1)
-node2 = Node(2)
-
-singlyLinkedList.head = node1
-singlyLinkedList.head.next = node2
-singlyLinkedList.tail = node2
--- recursion/factorial.py
+++ recursion/factorial.py
@@ -1,8 +1,27 @@
-def factorial(n):
-    assert n >= 0 and int(n) == n, 'The number must be a positive integer' # edge case
-    if n in [0, 1]:  # Base Case
-        return 1
-    else:
-        return n * factorial(n - 1) # recursive call
+# def factorial(n):
+#     assert n >= 0 and int(n) == n, 'The number must be a positive integer' # edge case
+#     if n in [0, 1]:  # Base Case
+#         return 1
+#     else:
+#         return n * factorial(n - 1) # recursive call
 
-print(factorial(4)) # 24
+# print(factorial(4)) # 24
+
+# def recursiveRange(num):
+
+#   # Base case
+#   if num <= 0:
+#     return 0
+#   else:
+#     return num + recursiveRange(num -1)
+
+# print(recursiveRange(6))
+def reverse(string):
+    
+  if len(string) <= 1:
+    return string
+  else:
+    return reverse(string[1:]) + string[0]
+
+
+print(reverse('taco'))
