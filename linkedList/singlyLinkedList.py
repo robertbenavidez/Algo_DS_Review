32687533325120cd0f1905125b2a82a14cdc09a5
Robert Benavidez <murabitbenavidez@gmail.com>
2021-05-12 15:23:53 -0400

    search for value method with time and space big O

--- linkedList/singlyLinkedList.py
+++ linkedList/singlyLinkedList.py
@@ -55,10 +55,29 @@ class SLinkedList:
                 print(node.value)
                 node = node.next
 
+    # Search for a value in a SLL
+    # Time:  O(n)
+    # Space: O(1)
+    def searchSLL(self, nodeValue):
+        # checks if head exists
+        if self.head is None:                   # O(1)
+            return "The SLL does not exist"     # O(1)
+        else:
+            # sets variable node to self.head to help with the traversal 
+            node = self.head                    # O(1)
+            while node is not None:             # O(n)
+                # checks for target nodeValue
+                if node.value == nodeValue:
+                    return node.value           # O(1)
+                # moves the variable to the next node in the SLL
+                node = node.next                # O(1)
+            return "the value does not exist"   # O(1)
+
 singlyLinkedList = SLinkedList()
 singlyLinkedList.insertSLL(1, 1)
 singlyLinkedList.insertSLL(2, 1)
 singlyLinkedList.insertSLL(3, 1)
 singlyLinkedList.insertSLL(4, 1)
 print([node.value for node in singlyLinkedList])
-singlyLinkedList.traverseSLL()
+# singlyLinkedList.traverseSLL()
+print(singlyLinkedList.searchSLL(0))
