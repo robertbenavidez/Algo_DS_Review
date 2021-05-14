5bcd0d2889dbdc7a392b3e99a6cc31ecdc2515b8
Robert Benavidez <murabitbenavidez@gmail.com>
2021-05-13 23:48:55 -0400

    CSLL sanity check

--- linkedList/CircularSLL.py
+++ linkedList/CircularSLL.py
@@ -16,3 +16,17 @@ class CircularSLL:
             if node.next == self.head:
                 break
             node = node.next
+
+    # creates CSLL
+    def createCSLL(self, nodeValue):
+        node = Node(nodeValue)
+        node.next = node
+        self.head = node
+        self.tail = node
+        return "The CSLL has been created"
+
+
+circularSLL = CircularSLL()
+circularSLL.createCSLL(1)
+
+print([node.value for node in circularSLL])
