f680a90807aa5bb69fe57a143b8987835726fd6f
Robert Benavidez <murabitbenavidez@gmail.com>
2021-07-22 13:34:47 -0400

    insertion method for CSLL

--- linkedList/CircularSLL.py
+++ linkedList/CircularSLL.py
@@ -13,9 +13,9 @@ class CircularSLL:
         node = self.head
         while node:
             yield node
-            if node.next == self.head:
-                break
             node = node.next
+            if node.next == self.tail.next:
+                break
 
     # creates CSLL
     # Time: O(1) Space: O(1)
@@ -26,8 +26,39 @@ class CircularSLL:
         self.tail = node
         return "The CSLL has been created"
 
+# Insertion of a node in CSLL
+
+    def insertCSLL(self, value, location):
+        if self.head is None:
+            return "The head is None"
+        else:
+            newNode = Node(value)
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+                self.tail.next = newNode
+            elif location == 1:
+                newNode.next = self.tail.next
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+            return "the node has been succesfully inserted"
+
 
 circularSLL = CircularSLL()
-circularSLL.createCSLL(1)
+print(circularSLL.createCSLL(1))
+circularSLL.insertCSLL(0, 0)
+circularSLL.insertCSLL(2, 1)
+circularSLL.insertCSLL(2, 2)
+circularSLL.insertCSLL(3, 2)
+circularSLL.insertCSLL(1, 0)
 
 print([node.value for node in circularSLL])
