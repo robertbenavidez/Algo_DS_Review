458806e120b2519acdcc6d22f01f0017133ed7fb
Robert Benavidez <murabitbenavidez@gmail.com>
2021-08-09 22:27:00 -0400

    divide and conquer problems

---
+++ DivideAndConquer/numberFactor.py
@@ -0,0 +1,15 @@
+# Problem:
+# Given N, find the number of ways to express N 
+# as the sum of 1, 3, 4
+
+def numberFactor(n):
+    if n in (0,1,2):
+        return 1
+    elif n == 3:
+        return 2
+    else:
+        subP1 = numberFactor(n-1)
+        subP2 = numberFactor(n-3)
+        subP3 = numberFactor(n-4)
+        return subP1+subP2+subP3
+print(numberFactor(5))
--- Trees/BinaryTree.py
+++ Trees/BinaryTree.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, data):
         self.data = data
@@ -37,4 +40,78 @@ def postOrderTraversal(rootNode):
     postOrderTraversal(rootNode.rightChild)
     print(rootNode.data)
 
-postOrderTraversal(newBT)
+# def levelOrderTraversal(rootNode):
+#     if not rootNode:
+#         return
+#     custumQueue = [rootNode]
+    
+    
+
+#     while (not custumQueue):
+#         root = custumQueue.pop(0)
+#         print(root.data)
+#         if (root.data.leftChild is not None):
+#             custumQueue.append(root.data.leftChild)
+#         if (root.data.rightChild is not None):
+#             custumQueue.append(root.data.rightChild)
+# levelOrderTraversal(newBT)
+
+
+# postOrderTraversal(newBT)
+# Code taken from GeeksforGeeks
+# In the tutorial they say it is a two Queue implementation
+# but rather it is a two Stack implementation
+def levelOrder(node):
+        q1 = [] # Queue 1
+        q2 = [] # Queue 2
+        q1.append(node)
+         
+        """Executing loop until both the
+        queues become empty"""
+        while(len(q1) > 0 or len(q2) > 0):
+         
+            """Empty string to concatenate
+            the string for q1"""
+            concat_str_q1 = ''
+            while(len(q1) > 0):
+                 
+                """Popped node at the first
+                pos in queue 1 i.e q1"""
+                popped_node = q1.pop(0)
+                concat_str_q1 += popped_node.data +' '
+                 
+                """Pushing left child of current
+                node in first queue into second queue"""
+                if popped_node.leftChild:
+                    q2.append(popped_node.leftChild)
+                     
+                """Pushing right child of current node
+                in first queue into second queue"""
+                if popped_node.rightChild:
+                    q2.append(popped_node.rightChild)
+            print( str(concat_str_q1))
+            concat_str_q1 = ''
+             
+            """Empty string to concatenate the
+            string for q1"""
+            concat_str_q2 = ''
+            while (len(q2) > 0):
+             
+                """Poped node at the first pos
+                in queue 1 i.e q1"""
+                popped_node = q2.pop(0)
+                concat_str_q2 += popped_node.data + ' '
+                 
+                """Pushing left child of current node
+                in first queue into first queue"""
+                if popped_node.leftChild:
+                    q1.append(popped_node.leftChild)
+                 
+                """Pushing right child of current node
+                in first queue into first queue"""
+                if popped_node.rightChild:
+                    q1.append(popped_node.rightChild)
+            print(str(concat_str_q2))
+            concat_str_q2 = ''
+
+# levelOrder(newBT)
